0e1923c788771ae19c5afa3291b983944bad2037
Hassan Brahimi <hbrahimi@c1r3p5.1337.ma>
2024-07-02 12:02:25 +0100

    added

---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from datetime import datetime
+import uuid
+"""this model will contain the super class of everything"""
+
+
+class BaseModel:
+    """the super class of evtg"""
+    def __init__(self):
+        """the constructor method"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """updates the time of last modif"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns the dictionary representation of the obj"""
+        nary = self.__dict__.copy()
+        nary["__class__"] = self.__class__.__name__
+        nary["created_at"] = self.created_at.isoformat()
+        nary["updated_at"] = self.updated_at.isoformat()
+        return nary
