3ec204b30cb734279b00afb33c00f8f9b7ff19d5
Hassan BRAHIMI <107075554+HassanBRAHIMI@users.noreply.github.com>
2024-07-04 10:04:38 +0000

    added

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
 
     def __str__(self):
         """the __str__ method"""
-        return ("[{}] [{}] {}".format(self.__class__.__name__,
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
 
     def save(self):
