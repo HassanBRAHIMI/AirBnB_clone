e1166071abc29772dea072d92dea42f1f4fcb1ba
Hassan BRAHIMI <107075554+HassanBRAHIMI@users.noreply.github.com>
2024-07-05 11:07:24 +0000

    added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,9 +4,9 @@ import os
 """a model that will contain the class FileStorage"""
 
 
-class Filestorage:
+class FileStorage:
     """a class that will take care of serialization and desrialization"""
-    __file_path = "../json_files/first.json"
+    __file_path = "first.json"
     __objects = {}
 
     def all(self):
@@ -23,18 +23,22 @@ class Filestorage:
         convert an obj to a dictionary so that way it can be serailizable
         """
         return obj.__dict__
-
-    def from_dict_to_jsonstr(self, dict):
-        """as it's name says"""
-        return json.dumps(dict)
     
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        list_of = []
-            
+        dictionary_of_dicts = {}
+        for key, value in self.__objects.items():
+            dictionary_of_dicts[key] = self.to_dictionary(value)
         with open(self.__file_path, 'w') as f:
-            json.dump(self.__objects, f)
+            json.dump(dictionary_of_dicts, f)
         f.close()
+
+    def from_dictionary(self, dictionary):
+        for key, value in dictionary.items():
+            class_name = key.split(".", 0)
+        obj = eval(class_name)
+        obj.__dict__.update(value)
+        return obj
 
     def reload(self):
         """
@@ -43,4 +47,7 @@ class Filestorage:
         """
         if os.path.exists(self.__file_path):
             with open(self.__file_path, 'r') as f:
-                self.__objects = json.load(f)
+                dictionary_of_dicts = json.load(f)
+            f.close()
+            for key, value in dictionary_of_dicts.items():
+                self.__objects[key] = self.from_dictionary(value)
